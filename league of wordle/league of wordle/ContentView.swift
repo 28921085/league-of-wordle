343f01169c25c5533397df4e450378bd8b31432c
28921085 <83084203+28921085@users.noreply.github.com>
2022-04-01 23:40:33 +0800

    Add files via upload

--- league of wordle/league of wordle/ContentView.swift
+++ league of wordle/league of wordle/ContentView.swift
@@ -8,34 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var str:[String]=["ㄅㄆㄇㄈ","ㄉㄊㄋㄌ","#ㄍㄎㄏ","$ㄐㄑㄒ","ㄓㄔㄕㄖ","^ㄗㄘㄙ","ㄦㄧㄨㄩ","ㄚㄛㄜㄝ","ㄞㄟㄠㄡ","ㄢㄣㄤㄥ"]
+    @State private var str:[String]=["ㄅㄆㄇㄈ","ㄉㄊㄋㄌ","⇤ㄍㄎㄏ","⟳ㄐㄑㄒ","ㄓㄔㄕㄖ","↵ㄗㄘㄙ","ㄦㄧㄨㄩ","ㄚㄛㄜㄝ","ㄞㄟㄠㄡ","ㄢㄣㄤㄥ"]
     @State private var input:[String]=[]
     @State private var maxlen:Int=5
     @State private var len:Int=0
+    @State private var ans_zu:String=""
+    @State private var ans_ch:String=""
     var body: some View {
         VStack{
+            
+            Text("League Of Wordle").font(.largeTitle).position(x: 170, y: 0)
             HStack{
                /* ForEach(0..<len){ inp in
                     Text(input[inp])
                 }*/
-                ForEach(input.indices){i in
+                ForEach(input.indices,id:\.self){i in
                     Text(input[i])
                 }
             }
-            HStack(spacing:1){//keyboard
+            HStack(spacing:2){//keyboard
                 ForEach(0..<10){i in
-                    VStack(spacing:1){
+                    VStack(spacing:2){
                         //for j in str[i]{
                         ForEach(0..<4){j in
                           
                             Button(action:{
                                 let idx:String.Index=str[i].index(str[i].startIndex,offsetBy:j)
-                                input.append(""+str[i][idx...idx])
-                                len+=1
+                                if str[i][idx...idx] == "⇤"{
+                                    if len != 0{
+                                        len-=1
+                                        input.removeLast()
+                                    }
+                                }
+                                else if str[i][idx...idx] == "↵"{
+                                    if len == maxlen{
+                                        if let quiz=NSDataAsset(name: "quiz"){
+                                            
+                                        }
+                                    }
+                                }
+                                else if str[i][idx...idx] == "⟳"{
+                                    len=0
+                                    input=[]
+                                }
+                                else if len != maxlen{
+                                    input.append(""+str[i][idx...idx])
+                                    len+=1
+                                }
+                                
                             }){
                                 
                                 let idx:String.Index=str[i].index(str[i].startIndex,offsetBy:j)
-                                Text(""+str[i][idx...idx]).font(.system(size: 30))
+                                if str[i][idx...idx]=="⇤" || str[i][idx...idx]=="↵" || str[i][idx...idx]=="⟳"{
+                                    Text(""+str[i][idx...idx]).font(.system(size: 30)).foregroundColor(Color.gray)
+                                }
+                                else{
+                                    Text(""+str[i][idx...idx]).font(.system(size: 30)).background(Color.purple).foregroundColor(Color.white)
+                                }
+                                
                             }
                         }
                     }
