d3a27a7350c8ab013059813e3558f865b6aeda28
28921085 <83084203+28921085@users.noreply.github.com>
2022-04-02 23:38:58 +0800

    Add files via upload

--- league of wordle/league of wordle/ContentView.swift
+++ league of wordle/league of wordle/ContentView.swift
@@ -14,50 +14,90 @@ struct ContentView: View {
     @State private var len:Int=0
     @State private var ans_zu:String=""
     @State private var ans_ch:String=""
+    @State private var guess:Int=6
+    @State private var guess_now:Int=0
+    @State private var record:[[String]]=Array(repeating: Array(repeating: "  ", count: 5), count: 6)
+    @State private var alertTitle:String=""
+    @State private var warning:Bool=false
+    @State private var data:[[String]]=Array(repeating:[], count: 25)
     var body: some View {
         VStack{
-            
             Text("League Of Wordle").font(.largeTitle).position(x: 170, y: 0)
-            HStack{
-               /* ForEach(0..<len){ inp in
-                    Text(input[inp])
-                }*/
+                .onAppear{
+                    if let asset = NSDataAsset(name:"quiz"),
+                       let content = String(data: asset.data,encoding: .utf8){
+                        let array = content.split(separator: "\r\n")
+                        for i in array{
+                            let n = i.count
+                            data[n].append(String(i))
+                        }
+                        ans_zu = data[maxlen].randomElement()!
+                    }
+                    record=Array(repeating: Array(repeating: "  ", count: maxlen), count: guess)
+                }
+            Text(ans_zu)
+            VStack(spacing:5){
+                ForEach(0..<guess){i in
+                    HStack(spacing:5){
+                        ForEach(0..<maxlen){j in
+                            Rectangle().fill(Color.gray).frame(width: CGFloat(280/maxlen), height: CGFloat(280/maxlen))
+                                .overlay(Text(record[i][j]).font(.system(size:CGFloat(250/maxlen))))
+                        }
+                    }
+                }
+            }
+            /*HStack{
                 ForEach(input.indices,id:\.self){i in
                     Text(input[i])
                 }
-            }
+            }*/
             HStack(spacing:2){//keyboard
                 ForEach(0..<10){i in
                     VStack(spacing:2){
-                        //for j in str[i]{
                         ForEach(0..<4){j in
-                          
-                            Button(action:{
+                            Button{
                                 let idx:String.Index=str[i].index(str[i].startIndex,offsetBy:j)
                                 if str[i][idx...idx] == "⇤"{
                                     if len != 0{
                                         len-=1
-                                        input.removeLast()
+                                        record[guess_now][len]="  "
                                     }
                                 }
                                 else if str[i][idx...idx] == "↵"{
                                     if len == maxlen{
-                                        if let quiz=NSDataAsset(name: "quiz"){
-                                            
+                                        var str:String = record[guess_now].joined()
+                                        if str==ans_zu{
+                                            warning = true
+                                            alertTitle = "correct"
                                         }
+                                        else{
+                                            if let tmp = data[maxlen].first(where:{$0 == str}){
+                                                
+                                                guess_now+=1
+                                                len=0
+                                            }
+                                            else{
+                                                warning=true
+                                                alertTitle="input word not found"
+                                            }
+                                        }
+                                    }
+                                    else{
+                                        warning=true
+                                        alertTitle = "not enough words"
                                     }
                                 }
                                 else if str[i][idx...idx] == "⟳"{
                                     len=0
-                                    input=[]
+                                    record[guess_now] = Array(repeating: "  ", count: maxlen)
+                                    //input=[]
                                 }
                                 else if len != maxlen{
-                                    input.append(""+str[i][idx...idx])
+                                    //input.append(""+str[i][idx...idx])
+                                    record[guess_now][len] = (""+str[i][idx...idx])
                                     len+=1
                                 }
-                                
-                            }){
-                                
+                            }label:{
                                 let idx:String.Index=str[i].index(str[i].startIndex,offsetBy:j)
                                 if str[i][idx...idx]=="⇤" || str[i][idx...idx]=="↵" || str[i][idx...idx]=="⟳"{
                                     Text(""+str[i][idx...idx]).font(.system(size: 30)).foregroundColor(Color.gray)
@@ -65,8 +105,10 @@ struct ContentView: View {
                                 else{
                                     Text(""+str[i][idx...idx]).font(.system(size: 30)).background(Color.purple).foregroundColor(Color.white)
                                 }
-                                
                             }
+                            .alert(alertTitle,isPresented:$warning,actions:{
+                                Button("OK"){}
+                            })
                         }
                     }
                 }
